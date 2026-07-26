caffa531b3dad92412a0d2a889eca2e14a47279c
AhmadMuzayyin <muzayyin654@gmail.com>
2026-07-26 14:31:03 +0700

    feat: cleaning folder for v2 preparation

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\RFIDController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::controller(RFIDController::class)->group(function () {
-    Route::post('/rfid/scan', 'scan')->name('rfid.scan');
-});
